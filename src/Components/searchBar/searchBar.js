9db012ddc5706e8897112bd4360a174c0d23dd1f
BirkofNiki <augastinendeti@gmail.com>
2024-05-01 16:47:10 +0300

    started the Track component and at this point completed its Track.css file

---
+++ src/Components/searchBar/searchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./searchBar.css";
+
+class searchBar extends React.Element{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         term:""
+      }; //A constructor passing props to the super class constructor
+      this.handleTermChange= this.handleTermChange.bind(this);
+      this.search= this.search.bind(this);
+      this.handleEnter = this.handleEnter.bind(this);      
+
+    }
+    {/*end of creating the searchBar Functions, Now I define each below*/}
+
+    handleTermChange(event){
+        this.setState({term:event.target.value});
+    }
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+    handleEnter(event){
+        if(event.keyCode === 13){
+            this.search();
+        } {/*keyCode === 13 is the data that the user enters, e.g here i3 means pressing the enter Key*/}
+    }
+
+
+    render(){
+        return(
+            <div className= "searchBar">
+            <input placeholder="Enter Song, album, or artist" onChange={this.handleTermChange} onKeyUp={this.handleEnter} />
+            <button className="searchButton" onclick={this.search}>SEARCH </button>
+            </div>
+        );
+    }
+}
+
+export default searchBar;
